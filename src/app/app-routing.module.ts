427c6aff455d0286932275aa12f560c6fb2bf182
svasquez <santivasquez1@gmail.com>
2021-05-01 14:36:21 -0500

    Creacion de rutas y componentes del portafolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateProjectComponent } from './components/create-project/create-project.component';
+import { ContactComponent } from './components/contact/contact.component'
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AboutComponent},
+  {path:'sobremi',component:AboutComponent},
+  {path:'proyectos',component:ProjectsComponent},
+  {path:'crearProjecto',component:CreateProjectComponent},
+  {path:'contacto',component:ContactComponent},
+  {path:'**',component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AboutComponent} from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateProjectComponent } from './components/create-project/create-project.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateProjectComponent,
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
